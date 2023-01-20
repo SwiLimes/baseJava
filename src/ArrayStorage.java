2b95378570922104d3b43ab9a027d51f06c82a69
Nikita Berdennikov <nberdennikov@blogic.ru>
2023-01-20 20:37:14 +0300

    Прописал логику работы с массивом для хранения резюме.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,70 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        System.out.println(r);
+        if (r.uuid == null) {
+            return;
+        }
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        if (size == 0) {
+            return;
+        }
+
+        if (uuid.equals("")) {
+            return;
+        }
+
+        int indexDelElement = -1;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                indexDelElement = i;
+            }
+        }
+
+        if (indexDelElement == -1) {
+            return;
+        }
+
+        for (int i = indexDelElement + 1; i < size; i++) {
+            storage[i - 1] = storage[i];
+        }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
